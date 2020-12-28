4fbe130ab15e0257ef08aa2266233d56e59e4af5
David Francis <dm.francis@live.co.uk>
2020-12-28 23:43:43 +0000

    Add day 16

---
+++ Day16/FieldDefinition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16
+{
+    public class FieldDefinition
+    {
+        public string Name { get; }
+        public ReadOnlyCollection<int> ValidValues { get; }
+
+        public FieldDefinition(string fieldRaw)
+        {
+            string[] fieldSections = fieldRaw.Split(": ");
+            Name = fieldSections[0];
+
+            string[] ranges = fieldSections[1].Split(" or ");
+
+            var validValues = new List<int>();
+            foreach(string range in ranges)
+            {
+                string[] minMaxStr = range.Split('-');
+                int min = int.Parse(minMaxStr[0]);
+                int max = int.Parse(minMaxStr[1]);
+
+                validValues.AddRange(Enumerable.Range(min, max - min + 1));
+            }
+
+            ValidValues = validValues.AsReadOnly();
+        }
+    }
+}
---
+++ Day16/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16
+{
+    public class Ticket
+    {
+        public ReadOnlyCollection<int> Values { get; }
+
+        public Ticket(string rawTicket)
+        {
+            Values = rawTicket.Split(',').Select(s => int.Parse(s)).ToList().AsReadOnly();
+        }
+    }
+}

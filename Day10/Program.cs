4a7f71c99a1a1df798213149e52bfc9ce8d7d9ab
David Francis <dm.francis@live.co.uk>
2021-12-12 16:09:25 +0000

    Days 10 and 11

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,2 +1,116 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var data = File.ReadAllLines("input.txt");
+
+
+Dictionary<char, char> openToClose = new()
+{
+    { '(', ')' },
+    { '[', ']' },
+    { '{', '}' },
+    { '<', '>' },
+};
+
+Dictionary<char, char> closeToOpen = openToClose.ToDictionary(kv => kv.Value, kv => kv.Key);
+
+
+var illegalCharacters = data.Select(line => GetFirstIllegalCharacter(line)).Where(ch => ch is not null);
+
+int totalSyntaxErrorScore = illegalCharacters.Sum(ch =>
+{
+    return ch switch
+    {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        _ => 0
+    };
+});
+
+Console.WriteLine($"Total syntax error score: {totalSyntaxErrorScore}");
+
+var completionStrings = new List<string>();
+foreach(string line in data)
+{
+    if (!IsIllegal(line))
+    {
+        string completionString = GetCompletionString(line);
+        completionStrings.Add(completionString);
+    }
+}
+
+List<long> orderedScores = completionStrings.Select(s => GetCompletionStringScore(s)).OrderBy(s => s).ToList();
+
+long middleScore = orderedScores[orderedScores.Count / 2];
+
+Console.WriteLine($"Middle score is {middleScore}");
+
+
+bool IsIllegal(string line) => GetFirstIllegalCharacter(line) != null;
+
+char? GetFirstIllegalCharacter(string line)
+{
+    var openChunks = new Stack<char>();
+
+    foreach (char ch in line)
+    {
+        if (openToClose.ContainsKey(ch))
+        {
+            openChunks.Push(ch);
+        }
+        else if (openChunks.Peek() == closeToOpen[ch])
+        {
+            openChunks.Pop();
+        }
+        else
+        {
+            return ch;
+        }
+    }
+
+    return null;
+}
+
+string GetCompletionString(string line)
+{
+    var openChunks = new Stack<char>();
+
+    foreach (char ch in line)
+    {
+        if (openToClose.ContainsKey(ch))
+        {
+            openChunks.Push(ch);
+        }
+        else if (openChunks.Peek() == closeToOpen[ch])
+        {
+            openChunks.Pop();
+        }
+        else
+        {
+            throw new ArgumentException($"Line is invalid. Expected {openToClose[openChunks.Peek()]}, but found {ch}", nameof(line));
+        }
+    }
+
+    char[] closingValues = openChunks.Select(x => openToClose[x]).ToArray();
+
+    return new string(closingValues);
+}
+
+static long GetCompletionStringScore(string completionString)
+{
+    long score = 0;
+
+    foreach (char ch in completionString)
+    {
+        score *= 5;
+        score += ch switch
+        {
+            ')' => 1,
+            ']' => 2,
+            '}' => 3,
+            '>' => 4,
+            _ => throw new InvalidOperationException($"Invalid character in completion string: {ch}")
+        };
+    }
+
+    return score;
+}
---
+++ Day11/Program.cs
@@ -0,0 +1,174 @@
+﻿var data = File.ReadAllLines("input.txt");
+
+var testData = new string[]
+{
+    "5483143223",
+    "2745854711",
+    "5264556173",
+    "6141336146",
+    "6357385478",
+    "4167524645",
+    "2176841721",
+    "6882881134",
+    "4846848554",
+    "5283751526"
+};
+
+
+int[,] grid = GetGridFromInput(data);
+
+int totalFlashes = 0;
+
+Console.WriteLine("Before any steps:");
+RenderGrid(grid);
+for (int i = 0; i < 100; i++)
+{
+    totalFlashes += ApplyStepToGrid(grid);
+
+    Console.WriteLine($"\nAfter step {i + 1}:");
+    RenderGrid(grid);
+}
+
+Console.WriteLine($"Total flashes after 100 steps: {totalFlashes}");
+
+
+int[,] grid2 = GetGridFromInput(data);
+int flashes;
+int steps = 0;
+do
+{
+    flashes = ApplyStepToGrid(grid2);
+    steps++;
+}
+while (flashes != 100);
+
+Console.WriteLine($"First step where all Octopuses flash: {steps}");
+
+static int[,] GetGridFromInput(string[] input)
+{
+    var map = new int[input.Length, input[0].Length];
+
+    for (int row = 0; row < input.Length; row++)
+    {
+        for (int col = 0; col < input[0].Length; col++)
+        {
+            map[row, col] = int.Parse(input[row][col].ToString());
+        }
+    }
+
+    return map;
+}
+
+static int ApplyStepToGrid(int[,] grid)
+{
+    int rowLength = grid.GetLength(0);
+    int colLength = grid.GetLength(1);
+
+    bool[,] flashed = new bool[rowLength, colLength];
+
+    // Increase all energy levels by 1
+    for (int row = 0; row < rowLength; row++)
+    {
+        for (int col = 0; col < colLength; col++)
+        {
+            grid[row, col]++;
+        }
+    }
+
+    int totalFlashCount = 0;
+    int flashCount;
+    do
+    {
+        flashCount = ProcessFlashes(grid, flashed);
+        totalFlashCount += flashCount;
+    }
+    while (flashCount > 0);
+
+
+    // Set flashed octopuses to 0
+    for (int row = 0; row < rowLength; row++)
+    {
+        for (int col = 0; col < colLength; col++)
+        {
+            if (flashed[row, col])
+                grid[row, col] = 0;
+        }
+    }
+
+    return totalFlashCount;
+}
+
+static int ProcessFlashes(int[,] grid, bool[,] flashed)
+{
+    int rowLength = grid.GetLength(0);
+    int colLength = grid.GetLength(1);
+    int rowMax = rowLength - 1;
+    int colMax = colLength - 1;
+
+    int flashCount = 0;
+
+    for (int row = 0; row < rowLength; row++)
+    {
+        for (int col = 0; col < colLength; col++)
+        {
+            if (grid[row, col] > 9 && !flashed[row, col])
+            {
+                foreach(var coord in GetAdjacentCoordinates(row, col, rowMax, colMax))
+                {
+                    grid[coord.Row, coord.Col]++;
+                }
+
+                flashed[row, col] = true;
+                flashCount++;
+            }
+        }
+    }
+
+    return flashCount;
+}
+
+static IEnumerable<(int Row, int Col)> GetAdjacentCoordinates(int row, int col, int rowMax, int colMax)
+{
+    if (row != 0)
+        yield return (row - 1, col);
+
+    if (col != 0)
+        yield return (row, col - 1);
+
+    if (row != rowMax)
+        yield return (row + 1, col);
+
+    if (col != colMax)
+        yield return (row, col + 1);
+
+    if (row != 0 && col != 0)
+        yield return (row - 1, col - 1);
+
+    if (row != 0 && col != colMax)
+        yield return (row - 1, col + 1);
+
+    if (row != rowMax && col != 0)
+        yield return (row + 1, col - 1);
+
+    if (row != rowMax && col != colMax)
+        yield return (row + 1, col + 1);
+}
+
+
+static void RenderGrid(int[,] grid)
+{
+    Console.ResetColor();
+    for (int row = 0; row < grid.GetLength(0); row++)
+    {
+        for (int col = 0; col < grid.GetLength(1); col++)
+        {
+            if (grid[row,col] == 0)
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write(grid[row, col]);
+            Console.ResetColor();
+        }
+
+        Console.WriteLine();
+    }
+}

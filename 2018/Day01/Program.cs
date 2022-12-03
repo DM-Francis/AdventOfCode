3e8d28de8ddb1b6df8a3d423464fbe2809ada56c
David Francis <dm.francis@live.co.uk>
2022-12-03 12:37:42 +0000

    Add 2018 day 1

---
+++ 2018/Day01/Program.cs
@@ -0,0 +1,34 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+// Part 1
+int frequency = 0;
+foreach (var change in input)
+{
+    int frequencyChange = int.Parse(change);
+    frequency += frequencyChange;
+}
+
+Console.WriteLine($"Final frequency: {frequency}");
+
+// Part 2
+int duplicateFrequency = GetFirstDuplicateFrequency(input);
+
+Console.WriteLine($"First duplicate frequency: {duplicateFrequency}");
+
+static int GetFirstDuplicateFrequency(string[] input)
+{
+    int frequency = 0;
+    var allFrequencies = new HashSet<int> {frequency};
+
+    while (true)
+    {
+        foreach (var change in input)
+        {
+            frequency += int.Parse(change);
+            if (allFrequencies.Contains(frequency))
+                return frequency;
+
+            allFrequencies.Add(frequency);
+        }
+    }
+}

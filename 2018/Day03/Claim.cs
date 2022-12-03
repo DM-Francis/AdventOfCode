79be7924f728e802128ddf5478bf492bae6ec226
David Francis <dm.francis@live.co.uk>
2022-12-03 18:18:18 +0000

    Add 2018 day 3

---
+++ 2018/Day03/Claim.cs
@@ -0,0 +1,29 @@
+﻿namespace Day03;
+
+public record Claim(
+    int Id,
+    int FromLeft,
+    int FromTop,
+    int Width,
+    int Height)
+{
+    public static Claim FromString(string claimString)
+    {
+        string[] claimSplit = claimString.Split(" ");
+        string idString = claimSplit[0];
+        string positionString = claimSplit[2];
+        string sizeString = claimSplit[3];
+
+        int id = int.Parse(idString[1..]);
+
+        var positionInts = positionString[..^1].Split(',').Select(int.Parse).ToList();
+        int fromLeft = positionInts[0];
+        int fromRight = positionInts[1];
+
+        var sizeInts = sizeString.Split('x').Select(int.Parse).ToList();
+        int width = sizeInts[0];
+        int height = sizeInts[1];
+
+        return new Claim(id, fromLeft, fromRight, width, height);
+    }
+}
---
+++ 2018/Day03/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using Day03;
+
+var input = File.ReadAllLines("input.txt");
+var claims = input.Select(Claim.FromString).ToList();
+
+var grid = new Dictionary<(int X, int Y), List<int>>();
+
+
+foreach (var claim in claims)
+{
+    AddClaimToGrid(grid, claim);
+}
+
+// Part 1
+int overlapCount = grid.Values.Count(ids => ids.Count > 1);
+
+Console.WriteLine($"Overlap count: {overlapCount}");
+
+// Part 2
+var overlapsPerId = new Dictionary<int, int>(claims.Select(c => new KeyValuePair<int, int>(c.Id, 0)));
+
+foreach (var squareIds in grid.Values)
+{
+    if (squareIds.Count == 1)
+        continue;
+    
+    foreach (int id in squareIds)
+    {
+        overlapsPerId[id] += 1;
+    }
+}
+
+int idWithNoOverlaps = overlapsPerId.Single(kv => kv.Value == 0).Key;
+
+Console.WriteLine($"Id with no overlaps: {idWithNoOverlaps}");
+
+
+static void AddClaimToGrid(IDictionary<(int X, int Y), List<int>> grid, Claim claim)
+{
+    for (int x = claim.FromLeft; x < claim.FromLeft + claim.Width; x++)
+    {
+        for (int y = claim.FromTop; y < claim.FromTop + claim.Height; y++)
+        {
+            if (grid.TryGetValue((x, y), out List<int>? currentIds))
+                currentIds.Add(claim.Id);
+            else
+                grid[(x, y)] = new List<int> {claim.Id};
+        }
+    }
+}

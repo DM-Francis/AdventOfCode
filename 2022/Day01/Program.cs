284bca85cea0b9a8b0d30cf48d47e7dcf7fd4b04
David Francis <dm.francis@live.co.uk>
2022-12-03 10:50:15 +0000

    Add 2022 day 1

---
+++ 2022/Day01/Program.cs
@@ -0,0 +1,16 @@
+﻿string input = File.ReadAllText("input.txt");
+
+string[] splitByElf = input.Split("\n\n");
+
+var caloriesPerElf = splitByElf.Select(elfFood =>
+{
+    var items = elfFood.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+    return items.Sum(int.Parse);
+}).ToList();
+
+int maxCalories = caloriesPerElf.Max();
+var top3ElfCalories = caloriesPerElf.OrderByDescending(x => x).Take(3).ToList();
+
+Console.WriteLine($"Max calories: {maxCalories}");
+Console.WriteLine($"Top 3 calories: {string.Join(',', top3ElfCalories)}");
+Console.WriteLine($"Top 3 total calories: {top3ElfCalories.Sum()}");

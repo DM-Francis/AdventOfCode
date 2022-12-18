cca72c608afbb89bf7134ef75f8379f2bf823df1
David Francis <dm.francis@live.co.uk>
2022-12-18 17:08:16 +0000

    Re-write code for 2021 day 16

--- 2021/Day16/Program.cs
+++ 2021/Day16/Program.cs
@@ -1,84 +1,193 @@
-﻿string dataHex = File.ReadAllLines("input.txt")[0];
-string testData = "EE00D40C823060";
+﻿using System.Collections;
 
+string dataHex = File.ReadAllLines("input.txt")[0];
+string testData = "A0016C880162017C3686B18A3D4780";
 
-ReadData(Convert.FromHexString(testData));
+var dataBytes = Convert.FromHexString(dataHex);
+var result = ParsePacketTreeFromData(dataBytes);
 
-static void ReadData(byte[] rawData)
+// Part 1
+int sumOfVersionNumbers = result.GetVersionNumberSum();
+Console.WriteLine($"Sum of version numbers: {sumOfVersionNumbers}");
+
+// Part 2
+ulong evaluatedExpression = result.GetValue();
+Console.WriteLine($"Evaluated expression: {evaluatedExpression}");
+
+static Packet ParsePacketTreeFromData(byte[] data)
 {
-    int version = rawData[0] >> 5;
-    int typeId = (rawData[0] & 0b_00011100) >> 2;
+    var preparedBytes = data.Select(ReverseBitsInByte).ToArray();
+    var bitArray = new BitArray(preparedBytes);
 
-    if (typeId == 4) // Literal packet
-    {
-        byte[] literalData = (byte[])rawData.Clone();
-        literalData[0] &= 0b_00000011;  // Remove header bits
-        ReadLiteralValue(literalData);
-    }
-    else // Operator packet
-    {
-        int lengthTypeId = (rawData[0] & 0b_00000010) >> 1;
+    int index = 0;
+    return ParseNextPacket(bitArray, ref index);
+}
 
-        if (lengthTypeId == 0)
-        {
-            int dataLengthBits = ((rawData[0] & 1) << 14) + (rawData[1] << 13) + ((rawData[2] & 0b11111100) >> 2);  // Next 15 bits
-        }
-        else if (lengthTypeId == 1)
-        {
-            int dataLengthPackets = ((rawData[0] & 1) << 10) + (rawData[1] << 9) + ((rawData[2] & 0b11000000) >> 6); // Next 11 bits
-        }
+static byte ReverseBitsInByte(byte value)
+{
+    int output = 0;
+    output |= (value & 1) << 7;
+    output |= (value & 2) << 5;
+    output |= (value & 4) << 3;
+    output |= (value & 8) << 1;
+    output |= (value & 16) >> 1;
+    output |= (value & 32) >> 3;
+    output |= (value & 64) >> 5;
+    output |= (value & 128) >> 7;
+
+    return (byte)output;
+}
+
+static Packet ParseNextPacket(BitArray bitArray, ref int currentIndex)
+{
+    int version = (int)BitsToUInt64(bitArray, ref currentIndex, 3);
+    int typeId = (int)BitsToUInt64(bitArray, ref currentIndex, 3);
+
+    switch (typeId)
+    {
+        case 4:
+            ulong value = GetLiteralValueFromBitArray(bitArray, ref currentIndex);
+            return new LiteralPacket(version, value);
+        default:
+            int lengthTypeId = (int)BitsToUInt64(bitArray, ref currentIndex, 1);
+            IEnumerable<Packet> subPackets;
+            switch (lengthTypeId)
+            {
+                case 0:
+                {
+                    int length = (int)BitsToUInt64(bitArray, ref currentIndex, 15);
+                    subPackets = ParsePacketsInNextNBits(bitArray, ref currentIndex, length);
+                    break;
+                }
+                case 1:
+                {
+                    int length = (int)BitsToUInt64(bitArray, ref currentIndex, 11);
+                    subPackets = ParseNextNPackets(bitArray, ref currentIndex, length);
+                    break;
+                }
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(lengthTypeId), lengthTypeId, "Invalid length type id");
+            };
+
+            var packet = new OperatorPacket(version, typeId);
+            packet.ChildPackets.AddRange(subPackets);
+            return packet;
     }
 }
 
-
-static int ReadLiteralValue(byte[] literalData)
+static IEnumerable<Packet> ParsePacketsInNextNBits(BitArray bitArray, ref int currentIndex, long n)
 {
-    bool readingDataBits = false;
-    bool started = false;
-    bool expectingMoreGroups = true;
-    
-    int literalValue = 0;
-    int dataBitIndex = 0;
-    int bitGroupIndex = 0;
-
-    while (dataBitIndex < literalData.Length * 8)
+    var packets = new List<Packet>();
+    int startIndex = currentIndex;
+    while (currentIndex < startIndex + n)
     {
-        int currentBit = literalData[dataBitIndex / 8] & (1 << (7 - dataBitIndex % 8));  // Extract only the bit for this loop - will either be 0 or a power of 2
-
-        currentBit = Convert.ToInt32(currentBit > 0); // Now either 1 or 0
-
-        if (!started)
-        {
-            if (currentBit == 1)
-            {
-                readingDataBits = true;
-                started = true;
-            }
-        }
-        else if (readingDataBits)
-        {
-            literalValue = (literalValue << 1) + currentBit;
-            bitGroupIndex++;
-
-            if (bitGroupIndex == 4)
-            {
-                if (!expectingMoreGroups)
-                    break;
-
-                bitGroupIndex = 0;
-                readingDataBits = false;
-            }
-        }
-        else
-        {
-            readingDataBits = true;
-
-            if (currentBit == 0)
-                expectingMoreGroups = false;
-        }
-
-        dataBitIndex++;
+        packets.Add(ParseNextPacket(bitArray, ref currentIndex));
     }
 
-    return literalValue;
+    if (currentIndex > startIndex + n)
+        throw new InvalidOperationException("Used more bits than expected");
+
+    return packets;
+}
+
+static IEnumerable<Packet> ParseNextNPackets(BitArray bitArray, ref int currentIndex, long n)
+{
+    var packets = new List<Packet>();
+    for (int i = 0; i < n; i++)
+    {
+        packets.Add(ParseNextPacket(bitArray, ref currentIndex));
+    }
+
+    return packets;
+}
+
+static ulong GetLiteralValueFromBitArray(BitArray bitArray, ref int currentIndex)
+{
+    bool keepParsingGroups = true;
+    var outputBits = new List<bool>();
+    while (keepParsingGroups)
+    {
+        keepParsingGroups = bitArray[currentIndex++];
+        for (int i = 0; i < 4; i++)
+        {
+            outputBits.Add(bitArray[currentIndex++]);
+        }
+    }
+
+    var outputBitArray = new BitArray(outputBits.ToArray());
+    int tempIndex = 0;
+    return BitsToUInt64(outputBitArray, ref tempIndex, outputBits.Count);
+}
+
+static ulong BitsToUInt64(BitArray bitArray, ref int currentIndex, int length)
+{
+    if (length > 64)
+        throw new ArgumentOutOfRangeException(nameof(length), length, "Length must be less than or equal to 32");
+    
+    ulong output = 0;
+    for (int i = 0; i < length; i++)
+    {
+        ulong bit = bitArray[currentIndex + i] ? (ulong)1 : 0;
+        output |= bit << (length - i - 1);
+    }
+
+    currentIndex += length;
+    return output;
+}
+
+public abstract class Packet
+{
+    public int Version { get; protected set; }
+    public abstract int TypeId { get; }
+
+    public abstract int GetVersionNumberSum();
+    public abstract ulong GetValue();
+}
+
+public class LiteralPacket : Packet
+{
+    public override int TypeId => 4;
+    public ulong Value { get; }
+
+    public LiteralPacket(int version, ulong value)
+    {
+        Version = version;
+        Value = value;
+    }
+
+    public override int GetVersionNumberSum() => Version;
+    public override ulong GetValue() => Value;
+}
+
+public class OperatorPacket : Packet
+{
+    public override int TypeId { get; }
+    public List<Packet> ChildPackets { get; } = new();
+
+    public OperatorPacket(int version, int typeId)
+    {
+        Version = version;
+        TypeId = typeId;
+    }
+    
+    public override int GetVersionNumberSum()
+    {
+        return Version + ChildPackets.Sum(c => c.GetVersionNumberSum());
+    }
+
+    public override ulong GetValue()
+    {
+        return TypeId switch
+        {
+            0 => ChildPackets.Aggregate((ulong)0, (agg, p) => agg + p.GetValue()),
+            1 => ChildPackets.Aggregate((ulong)1, (agg, p) => agg * p.GetValue()),
+            2 => ChildPackets.Aggregate(ulong.MaxValue, (agg, p) => Math.Min(agg, p.GetValue())),
+            3 => ChildPackets.Aggregate((ulong)0, (agg, p) => Math.Max(agg, p.GetValue())),
+            5 => ChildPackets[0].GetValue() > ChildPackets[1].GetValue() ? (ulong)1 : 0,
+            6 => ChildPackets[0].GetValue() < ChildPackets[1].GetValue() ? (ulong)1 : 0,
+            7 => ChildPackets[0].GetValue() == ChildPackets[1].GetValue() ? (ulong)1 : 0,
+            _ => throw new ArgumentOutOfRangeException(nameof(TypeId), TypeId,
+                "Invalid TypeId value for operator packet")
+        };
+    }
 }

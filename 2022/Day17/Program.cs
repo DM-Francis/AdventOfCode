6a360415537c573e62fbd8a2b0167297e6265337
David Francis <dm.francis@live.co.uk>
2022-12-18 00:22:45 +0000

    Add part 2

--- 2022/Day17/Program.cs
+++ 2022/Day17/Program.cs
@@ -1,20 +1,49 @@
 ﻿using Day17;
 
 var wind = File.ReadAllText("input.txt").Trim('\n');
+// var wind = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
 var shapesInput = File.ReadAllText("shapes.txt");
 var individualShapeStrings = shapesInput.Split("\r\n\r\n");
 
 var shapes = individualShapeStrings.Select(Shape.FromString).ToList();
 
 int windIndex = 0;
-var chamber = new HashSet<Position>(); // All coordinates where there is a (stationary) piece of rock
+var chamber = new SortedSet<Position>(); // All coordinates where there is a (stationary) piece of rock
 
-for (int rockNumber = 0; rockNumber < 2022; rockNumber++)
+var heightOffsets = new Dictionary<long, int>();
+int baseHeight = 0;
+int repetitionEndHeight = 0;
+
+bool hasRepeated = false;
+
+int rockNumber = 0;
+int repetitionPeriodStart = 0;
+int repetitionPeriodEnd = 0;
+
+while (!hasRepeated)
 {
-    Console.WriteLine($"Simulating rock number: {rockNumber}");
+    switch (windIndex / wind.Length)
+    {
+        case 1 when repetitionPeriodStart == 0:
+            repetitionPeriodStart = rockNumber;
+            baseHeight = GetTowerHeight(chamber);
+            heightOffsets[0] = 0;
+            break;
+        case 1:
+            heightOffsets[rockNumber - repetitionPeriodStart] = GetTowerHeight(chamber) - baseHeight;
+            break;
+        case 2:
+            hasRepeated = true;
+            repetitionPeriodEnd = rockNumber;
+            repetitionEndHeight = GetTowerHeight(chamber) - baseHeight;
+            break;
+    }
+
     var currentRock = GenerateNextRock(rockNumber, chamber, shapes);
     bool isAtRest = false;
 
+    var startingPos = currentRock.Position;
+    
     while (!isAtRest)
     {
         currentRock = ApplyWindToRock(currentRock, chamber, wind[windIndex++ % wind.Length]);
@@ -27,21 +56,69 @@ for (int rockNumber = 0; rockNumber < 2022; rockNumber++)
     {
         chamber.Add(position);
     }
-    
-    // RenderChamberWithRock(chamber, currentRock);
+
+    var finishingPos = currentRock.Position;
+    int currentHeight = chamber.Max(p => p.Y);
+    Console.WriteLine($"Simulated rock number: {rockNumber + 1}.  Movement: ({finishingPos.X - startingPos.X}, {finishingPos.Y - startingPos.Y}).  Height: {currentHeight} Wind Index: {windIndex % wind.Length} Shape Index: {rockNumber % shapes.Count}");
+
+    rockNumber++;
 }
+
 
 int heightOfTower = chamber.Max(p => p.Y);
 Console.WriteLine($"Height of tower of 2022 rocks: {heightOfTower}");
 
-static Rock GenerateNextRock(int currentRockNumber, HashSet<Position> chamber, IReadOnlyList<Shape> shapes)
+// Part 2
+/*
+Investigate the results for repetitions.
+Found an interesting repetition once we get to the end of the wind inputs.
+
+Let n = rock number
+
+At n = 3399, Height = 5370 (+0 from the previous rock)
+
+The pattern repeats at n = 5094, Height = 8041 (again +0 from the previous rock)
+It repeats again at n = 6789, Height = 10712
+
+So, after a certain point, the pattern repeats every 1,695 rocks and the height increases by 2,671 each cycle
+
+If N = 3399 + k * 1695, then the height after N rocks, H(N) = 5370 + k * 2671
+
+For numbers between these 'multiples' of 1695 (e.g. with remainder r), we need to work out the height offset.
+So for any int N, let
+
+N = 3399 + k * 1695 + r, then the height after N rocks, H(N) = 5370 + k * 2671 + h(r)
+
+where h(r) is the height offset for the remainder.
+
+To find k and r:
+k = (N - 3399) / 1695 using integer division
+r = (N - 3399) % 1695
+
+Then to find H:
+
+H(N) = 5370 + k * 2671 + h(r)
+ */
+long period = repetitionPeriodEnd - repetitionPeriodStart;
+
+
+long N = 1000000000000;
+long k = (N - repetitionPeriodStart) / period;
+long r = (N - repetitionPeriodStart) % period;
+
+long height = baseHeight + k * repetitionEndHeight + heightOffsets[r];
+
+Console.WriteLine($"Height after {N} rocks: {height}");
+
+
+static Rock GenerateNextRock(int currentRockNumber, IEnumerable<Position> chamber, IReadOnlyList<Shape> shapes)
 {
     var highestRockY = chamber.DefaultIfEmpty().Max(p => p.Y);
     const int rockStartingX = 3;
     return new Rock(new Position(rockStartingX, highestRockY + 4), shapes[currentRockNumber % shapes.Count]);
 }
 
-static Rock ApplyWindToRock(Rock rock, HashSet<Position> chamber, char windDirection)
+static Rock ApplyWindToRock(Rock rock, IEnumerable<Position> chamber, char windDirection)
 {
     int xChange = windDirection switch
     {
@@ -57,7 +134,7 @@ static Rock ApplyWindToRock(Rock rock, HashSet<Position> chamber, char windDirec
     return newRockPosition;
 }
 
-static Rock ApplyFallToRock(Rock rock, HashSet<Position> chamber, out bool comeToRest)
+static Rock ApplyFallToRock(Rock rock, IEnumerable<Position> chamber, out bool comeToRest)
 {
     var movedDownRock = new Rock(rock.Position with { Y = rock.Position.Y - 1 }, rock.Shape);
 
@@ -72,13 +149,19 @@ static Rock ApplyFallToRock(Rock rock, HashSet<Position> chamber, out bool comeT
     return movedDownRock;
 }
 
-static void RenderChamberWithRock(HashSet<Position> chamber, Rock rock)
+static int GetTowerHeight(IEnumerable<Position> chamber)
+{
+    return chamber.DefaultIfEmpty().Max(p => p.Y);
+}
+
+static void RenderTopOfChamberWithRock(IReadOnlySet<Position> chamber, Rock rock)
 {
     const int wallRight = 8;
-    int maxY = Math.Max(rock.Position.Y + rock.Shape.Height - 1, chamber.DefaultIfEmpty().Max(p => p.Y));
+    int maxY = Math.Max(rock.Position.Y + rock.Shape.Height - 1, GetTowerHeight(chamber));
+    int minY = Math.Max(maxY - 40, 0);
 
     Console.Clear();
-    for (int y = maxY; y >= 0; y--)
+    for (int y = maxY; y >= minY; y--)
     {
         for (int x = 0; x <= wallRight; x++)
         {
@@ -94,10 +177,10 @@ static void RenderChamberWithRock(HashSet<Position> chamber, Rock rock)
                     Console.Write('|');
                     break;
                 default:
-                    if (rock.OverlapsWithPosition(new Position(x,y)))
-                        Console.Write('@');
-                    else if (chamber.Contains(new Position(x,y)))
+                    if (chamber.Contains(new Position(x,y)))
                         Console.Write('#');
+                    else if (rock.OverlapsWithPosition(new Position(x,y)))
+                        Console.Write('@');
                     else
                         Console.Write('.');
                     break;
@@ -108,7 +191,16 @@ static void RenderChamberWithRock(HashSet<Position> chamber, Rock rock)
     
 }
 
-public record struct Position(int X, int Y);
+public readonly record struct Position(int X, int Y) : IComparable<Position>
+{
+    public int CompareTo(Position other)
+    {
+        if (Y == other.Y)
+            return X.CompareTo(other.X);
+
+        return -Y.CompareTo(other.Y);
+    }
+}
 
 public class Rock
 {
@@ -139,6 +231,6 @@ public class Rock
 
     public bool OverlapsWithPosition(Position other)
     {
-        return _positions.Any(p => p == other);
+        return _positions.Contains(other);
     }
 }

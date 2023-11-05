f8519a5013ef31b3772d8e0a4b1903c9faad92be
David Francis <dm.francis@live.co.uk>
2023-11-05 20:22:42 +0000

    Complete the final day of 2020!

---
+++ 2020/Day25/Program.cs
@@ -0,0 +1,49 @@
+﻿var input = File.ReadAllLines("input");
+var exampleInput = """
+                   5764801
+                   17807724
+                   """.Split(Environment.NewLine);
+                   
+int cardPublicKey = int.Parse(input[0]);
+int doorPublicKey = int.Parse(input[1]);
+
+var results = CalculateLoopSizes(cardPublicKey, doorPublicKey, 7);
+int cardLoopSize = results[cardPublicKey];
+
+long encryptionKey = TransformForLoopSize(doorPublicKey, cardLoopSize);
+
+Console.WriteLine($"Encryption key: {encryptionKey}");
+
+return;
+
+
+static Dictionary<int,int> CalculateLoopSizes(int publicKey1, int publicKey2, int subjectNumber)
+{
+    var loopSizes = new Dictionary<int, int>();
+
+    long value = 1;
+    int loops = 0;
+    while (loopSizes.Count < 2)
+    {
+        value = (value * subjectNumber) % 20201227;
+        loops++;
+
+        if (value == publicKey1)
+            loopSizes[publicKey1] = loops;
+        else if (value == publicKey2)
+            loopSizes[publicKey2] = loops;
+    }
+
+    return loopSizes;
+}
+
+static long TransformForLoopSize(int subjectNumber, int loopSize)
+{
+    long value = 1;
+    for (int i = 0; i < loopSize; i++)
+    {
+        value = value * subjectNumber % 20201227;
+    }
+
+    return value;
+}
